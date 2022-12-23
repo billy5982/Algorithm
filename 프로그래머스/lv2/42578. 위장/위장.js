679bf0b3f71cbe370934b3f4509b73bbda0bb45e
billy5982 <104412610+billy5982@users.noreply.github.com>
2022-12-23 14:47:18 +0900

    [level 2] Title: 위장, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,9 +1,13 @@
 function solution(clothes) {
-    // 매일 다른 옷을 조합하여 자신을 위장
-    // 오늘 동그란 안경, 긴 코트, 파란색 티셔츠
-    // 다음날 (동그란 안경, 긴 코트, 파란색 티셔츠,청바지) || 선글라스 안경, 긴 코트, 파란색 티셔츠
-    // 스파이는 무조건 한개 이상의 의상을 입음
-    // [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+   /*
+   풀이 해석
+   1종류 옷이 있다 가정 -> 개수는 a개 -> 나올 수 있는 경우의 수 a가지
+   2종류 옷이 있다 가정 -> 개수는 a,b개 -> 나올 수 있는 경우의 수 (a+b)+(ab)
+   3종류 옷이 있다 가정 -> 개수는 a,b,c개 -> 나올 수 있는 경우의 수 (a+b+c) + (ab+ac+bc) + (abc)
+   (a+b+c) + (ab+ac+bc) + (abc) => x^3 + (a+b+c)^2 + (ab+bc+ac)x + (abc) x를 이용해서 치환가능
+   (x는 증명을 위한 수(곱을 위한 수))
+   (x+a)(x+b)(x+c) = x^3 + (a+b+c)x^2 + (ab+bc+ac)x + (abc) -> x=1 계산 후 x^3는 사용하지 않는 수니까 제거 
+   */
     const cloth = clothes.reduce((acc,cur)=>{
         if(acc[cur[1]]){
             acc[cur[1]]+=1
