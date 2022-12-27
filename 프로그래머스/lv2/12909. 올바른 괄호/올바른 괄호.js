6f55ad64489eac258b00eb0442b85d877f2faf02
billy5982 <104412610+billy5982@users.noreply.github.com>
2022-12-27 19:15:31 +0900

    [level 2] Title: 올바른 괄호, Time: 2.76 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -3,30 +3,22 @@ function solution(s){
     // (가 끝나는 순간 )의 숫자를 체크해야한다
     // [(,(], [),] => (()(() , ((, ))의 다음 거를 계속 알고있어야함 넣고 나서 비워줘야함
     // s='()())(()' False
-
-
-    if(s[0]===')')return false
-    let arr = [...s]
-    let openBracket = 0;
-    let closeBracket = 0;
+   
+    // 틀린이유 배열로 접근해서 unshift와 shift를 사용했기 떄문 또한, 열린괄호와 닫힌괄호로만 이용 마지막 예외 케이스에 걸렸을까?
+    // 짝을 맞추는 문제는 열린거 +1 닫힌 거 -1 하고 -1했을 때 음수로 내려가게 된다면 짝이 맞지 않는 것이기 때문에 
+ 
+    if(s[0]===')'||s.length<=1)return false
+    let balance = 0;
     
-    while(arr.length>0){
-        if(arr[0]==='('){ 
-            arr.shift()
-            openBracket+=1;
+    for(let i=0; i<s.length; i++){
+        if(s[i]==='('){ 
+            balance+=1;
         }else{ // 갇힌 괄호라면? 일단 해당 배열을 안에 넣고
-            arr.shift(); // 이상태에서는 )가 빠진 상태 다음 게 온 상태
-            closeBracket+=1;
-            if(arr[0]==='('){ // 열리는 괄호라면? 둘을 일단 비교
-                if(openBracket===closeBracket){ // 둘이 같다면 초기화 하고 다시 배열을 넣어주기
-                    openBracket = 0;
-                    closeBracket = 0;
-                }else{
-                    return false; // 길이가 같지 않다면 false 처리
-                }
+            balance-=1;
+            if(balance<0){
+                return false
             }
         }
     }
-    
-    return openBracket===closeBracket ? true : false
+    return balance!==0 ? false : true
 }
