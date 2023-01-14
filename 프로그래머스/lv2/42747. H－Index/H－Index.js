592ef92eea1c83ec971dbf134bee9f8eafa2253c
billy5982 <104412610+billy5982@users.noreply.github.com>
2023-01-14 10:56:15 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,13 @@
+function solution(citations) {
+    // 순번과 현재 수를 비교했을 때 같아지는 수 또는 끝까지 도달했을 때
+    const sortCitations = citations.sort((a,b)=>b-a)
+    let Hindex = 0;
+    for(let i =0; i<sortCitations.length; i++){
+        if(sortCitations[i]>=i+1&&i<=sortCitations.length-1){
+            Hindex++;
+            continue
+        }
+        break
+    }
+    return Hindex
+}
