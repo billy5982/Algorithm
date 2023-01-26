cd6bd951e60b8fa4324f86e19629fa81a80ff9ff
billy5982 <104412610+billy5982@users.noreply.github.com>
2023-01-26 18:35:13 +0900

    [level 2] Title: 피로도, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,42 +1,17 @@
 function solution(k, dungeons) {
-     let answer = -1;
-  const dfs = (k, dungeons, prev) => {
-    for (let i = 0; i < dungeons.length; i++) {
-      const [req, use] = dungeons[i];
-      if (!req || req > k) continue;
-      const copy = [...dungeons].map((v) => [...v]);
-      copy[i] = [null, null];
-      dfs(k - use, copy, prev + 1);
+    let answer = -1
+    function recursive(gauge,arr,cnt){  
+        for(let i =0; i<arr.length; i++){
+            const [limit,use] = arr[i]
+            if(!limit || limit>gauge) continue;
+            const copy = [...arr].map((el)=>[...el])
+            copy[i] = [null,null]
+            recursive(gauge-use,copy,cnt+1)
+        }
+        return (answer = Math.max(cnt,answer))
     }
-    return (answer = Math.max(prev, answer));
-  };
-  dfs(k, dungeons, 0);
-  return answer;
-    
-//     let answer = []
-//     let bools = new Array(dungeons.length).fill(false)
-    
-//     function recursive(gauge,count,getCnt){
-//         answer.push(count)
-//         for(let i =0; i<dungeons.length; i++){
-//             if(bools[i])continue
-//             if(gauge>=dungeons[i][0]){
-//                 bools[i] = true
-//                 recursive(k-dungeons[i][1],count+1,getCnt+1)
-//                 bools[i] = false
-//             }else{
-//                 return
-//             }
-//         }
-//     }
-    
-//     for(let j =0; j<dungeons.length; j++){
-//         if(k>=dungeons[j][0]){
-//         bools[j] = true;
-//         recursive(k-dungeons[j][1],1,1)
-//         bools[j] = false
-//         }
-//     }
-    
-//     return (Math.max(...answer))
+    recursive(k,dungeons,0)
+    return (answer)
 }
+
+
