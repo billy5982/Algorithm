ea13f45bb2fb874f3d558712a708a9750452fc40
billy5982 <104412610+billy5982@users.noreply.github.com>
2023-03-24 11:26:00 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __filename.split('/').pop().slice(0, -3) + '.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map((el) => +el);
+
+/*
+  1<= N 까지를 순회하며 길이가 M인 수열을 만들어야함
+  1,2,3  
+  1,3,2 => 중첩이 안되게 해야함. visit 필요
+  2,1,3
+*/
+const sol = (input) => {
+  const [N, M] = input;
+
+  const visit = new Array(N).fill(false);
+  const answer = [];
+  //  숫자계산 시 인덱스로 계산해야하기 때문에 방문지 확인은 -1을 이용
+  const dfs = (arr, cur) => {
+    if (arr.length === M) {
+      answer.push(arr.join(' '));
+      return;
+    }
+    for (let i = cur; i <= N; i++) {
+      // 방문한 노드라면
+      if (visit[i - 1]) continue;
+      visit[i - 1] = true;
+      dfs([...arr, i], i);
+      visit[i - 1] = false;
+    }
+  };
+
+  dfs([], 1);
+
+  return answer.join('\n');
+};
+
+console.log(sol(input));
