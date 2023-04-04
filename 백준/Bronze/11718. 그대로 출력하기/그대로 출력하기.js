b7d9979d8d579baec96203d1d75f8bf79adc481e
billy5982 <104412610+billy5982@users.noreply.github.com>
2023-04-04 22:24:42 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 132 ms, Memory: 9500 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __filename.split('/').pop().slice(0, -3) + '.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+for (let el of input) {
+  console.log(el);
+}
