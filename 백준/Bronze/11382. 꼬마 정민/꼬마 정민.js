cf3bbadebac9db3b6755b0cb26fcaf6904673f7d
billy5982 <104412610+billy5982@users.noreply.github.com>
2023-04-04 22:08:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __filename.split('/').pop().slice(0, -3) + '.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .reduce((acc, cur) => acc + +cur, 0);
+
+console.log(input);
