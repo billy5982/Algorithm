2cfdc0d402d1b5483c88308fe73d667c7229909d
billy5982 <104412610+billy5982@users.noreply.github.com>
2023-04-04 22:22:13 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 192 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : __filename.split('/').pop().slice(0, -3) + '.txt')
+  .toString()
+  .trim().length;
+console.log(input);
